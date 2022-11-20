9f483298f25fc101d8668b3a164fdf16336d1b8f
CT77777 <103264872+CT77777@users.noreply.github.com>
2022-11-20 12:07:17 +0800

    pass #constructor test

--- contracts/SimpleSwap.sol
+++ contracts/SimpleSwap.sol
@@ -6,7 +6,16 @@ import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SimpleSwap is ISimpleSwap, ERC20 {
     // Implement core logic here
-    constructor() ERC20("SimpleSwap Token", "SST") {}
+    constructor(address _tokenA, address _tokenB) ERC20("SimpleSwap Token", "SST") {
+        require(_tokenA != address(0), "SimpleSwap: TOKENA_IS_NOT_CONTRACT");
+        require(_tokenB != address(0), "SimpleSwap: TOKENB_IS_NOT_CONTRACT");
+        require(_tokenA != _tokenB, "SimpleSwap: TOKENA_TOKENB_IDENTICAL_ADDRESS");
+        tokenA = _tokenA;
+        tokenB = _tokenB;
+    }
+
+    address public tokenA;
+    address public tokenB;
 
     function swap(address tokenIn, address tokenOut, uint256 amountIn) external override returns (uint256 amountOut) {}
 
@@ -17,9 +26,15 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
 
     function removeLiquidity(uint256 liquidity) external override returns (uint256 amountA, uint256 amountB) {}
 
-    function getReserves() external view override returns (uint256 reserveA, uint256 reserveB) {}
+    function getReserves() external view override returns (uint256 reserveA, uint256 reserveB) {
+        return (ERC20(tokenA).balanceOf(address(this)), ERC20(tokenB).balanceOf(address(this)));
+    }
 
-    function getTokenA() external view override returns (address tokenA) {}
+    function getTokenA() external view override returns (address) {
+        return tokenA;
+    }
 
-    function getTokenB() external view override returns (address tokenB) {}
+    function getTokenB() external view override returns (address) {
+        return tokenB;
+    }
 }
