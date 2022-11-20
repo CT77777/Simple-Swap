4e5fc5dbfb13f4de24ca3e5b5925d6adcccae108
CT77777 <103264872+CT77777@users.noreply.github.com>
2022-11-20 17:44:55 +0800

    update #first time to add liquidity test

--- contracts/SimpleSwap.sol
+++ contracts/SimpleSwap.sol
@@ -3,8 +3,11 @@ pragma solidity 0.8.17;
 
 import { ISimpleSwap } from "./interface/ISimpleSwap.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
 
 contract SimpleSwap is ISimpleSwap, ERC20 {
+    using Math for uint256; // ^0.8 version already pre-check overflow
+
     // Implement core logic here
     constructor(address _tokenA, address _tokenB) ERC20("SimpleSwap Token", "SST") {
         require(_tokenA != address(0), "SimpleSwap: TOKENA_IS_NOT_CONTRACT");
@@ -22,7 +25,31 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
     function addLiquidity(
         uint256 amountAIn,
         uint256 amountBIn
-    ) external override returns (uint256 amountA, uint256 amountB, uint256 liquidity) {}
+    ) external override returns (uint256 amountA, uint256 amountB, uint256 liquidity) {
+        require(amountAIn != 0, "SimpleSwap: INSUFFICIENT_INPUT_AMOUNT");
+        require(amountBIn != 0, "SimpleSwap: INSUFFICIENT_INPUT_AMOUNT");
+
+        uint256 reserveA = ERC20(tokenA).balanceOf(address(this));
+        uint256 reserveB = ERC20(tokenB).balanceOf(address(this));
+
+        if (totalSupply() == 0) {
+            liquidity = Math.sqrt(amountAIn * amountBIn);
+            _mint(msg.sender, liquidity);
+            ERC20(tokenA).transferFrom(msg.sender, address(this), amountAIn);
+            ERC20(tokenB).transferFrom(msg.sender, address(this), amountBIn);
+        } else {
+            if (Math.ceilDiv(amountAIn, amountBIn) == Math.ceilDiv(reserveA, reserveB)) {
+                liquidity = Math.sqrt(amountAIn * amountBIn);
+                _mint(msg.sender, liquidity);
+                ERC20(tokenA).transferFrom(msg.sender, address(this), amountAIn);
+                ERC20(tokenB).transferFrom(msg.sender, address(this), amountBIn);
+            }
+        }
+
+        emit AddLiquidity(msg.sender, amountAIn, amountBIn, liquidity);
+
+        return (amountAIn, amountBIn, liquidity);
+    }
 
     function removeLiquidity(uint256 liquidity) external override returns (uint256 amountA, uint256 amountB) {}
 
@@ -36,5 +63,15 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
 
     function getTokenB() external view override returns (address) {
         return tokenB;
+    }
+
+    //approve token A allowance to SimpleSwap
+    function approveTokenA(uint256 _allowrance) external {
+        ERC20(tokenA).approve(address(this), _allowrance);
+    }
+
+    //approve token B allowance to SimpleSwap
+    function approveTokenB(uint256 _allowrance) external {
+        ERC20(tokenB).approve(address(this), _allowrance);
     }
 }
