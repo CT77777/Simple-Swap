65763f1ebf47a34222d7b163825122ab1c5d2934
CT77777 <103264872+CT77777@users.noreply.github.com>
2022-11-21 10:04:04 +0800

    update #not first time to add liquidity

--- contracts/SimpleSwap.sol
+++ contracts/SimpleSwap.sol
@@ -29,26 +29,51 @@ contract SimpleSwap is ISimpleSwap, ERC20 {
         require(amountAIn != 0, "SimpleSwap: INSUFFICIENT_INPUT_AMOUNT");
         require(amountBIn != 0, "SimpleSwap: INSUFFICIENT_INPUT_AMOUNT");
 
-        uint256 reserveA = ERC20(tokenA).balanceOf(address(this));
-        uint256 reserveB = ERC20(tokenB).balanceOf(address(this));
+        uint256 reserveACurrent = ERC20(tokenA).balanceOf(address(this));
+        uint256 reserveBCurrent = ERC20(tokenB).balanceOf(address(this));
+        uint256 actualAmountA;
+        uint256 actualAmountB;
 
         if (totalSupply() == 0) {
-            liquidity = Math.sqrt(amountAIn * amountBIn);
+            actualAmountA = amountAIn;
+            actualAmountB = amountBIn;
+            liquidity = Math.sqrt(actualAmountA * actualAmountB);
             _mint(msg.sender, liquidity);
-            ERC20(tokenA).transferFrom(msg.sender, address(this), amountAIn);
-            ERC20(tokenB).transferFrom(msg.sender, address(this), amountBIn);
+            ERC20(tokenA).transferFrom(msg.sender, address(this), actualAmountA);
+            ERC20(tokenB).transferFrom(msg.sender, address(this), actualAmountB);
         } else {
-            if (Math.ceilDiv(amountAIn, amountBIn) == Math.ceilDiv(reserveA, reserveB)) {
-                liquidity = Math.sqrt(amountAIn * amountBIn);
+            uint256 proportionTokenA = amountAIn / reserveACurrent;
+            uint256 proportionTokenB = amountBIn / reserveBCurrent;
+
+            if (proportionTokenA == proportionTokenB) {
+                actualAmountA = amountAIn;
+                actualAmountB = amountBIn;
+                liquidity = Math.sqrt(actualAmountA * actualAmountB);
                 _mint(msg.sender, liquidity);
-                ERC20(tokenA).transferFrom(msg.sender, address(this), amountAIn);
-                ERC20(tokenB).transferFrom(msg.sender, address(this), amountBIn);
+                ERC20(tokenA).transferFrom(msg.sender, address(this), actualAmountA);
+                ERC20(tokenB).transferFrom(msg.sender, address(this), actualAmountB);
+            }
+            if (proportionTokenA < proportionTokenB) {
+                actualAmountA = amountAIn;
+                actualAmountB = reserveBCurrent * (amountAIn / reserveACurrent);
+                liquidity = Math.sqrt(actualAmountA * actualAmountB);
+                _mint(msg.sender, liquidity);
+                ERC20(tokenA).transferFrom(msg.sender, address(this), actualAmountA);
+                ERC20(tokenB).transferFrom(msg.sender, address(this), actualAmountB);
+            }
+            if (proportionTokenA > proportionTokenB) {
+                actualAmountA = reserveACurrent * (amountBIn / reserveBCurrent);
+                actualAmountB = amountBIn;
+                liquidity = Math.sqrt(actualAmountA * actualAmountB);
+                _mint(msg.sender, liquidity);
+                ERC20(tokenA).transferFrom(msg.sender, address(this), actualAmountA);
+                ERC20(tokenB).transferFrom(msg.sender, address(this), actualAmountB);
             }
         }
 
-        emit AddLiquidity(msg.sender, amountAIn, amountBIn, liquidity);
+        emit AddLiquidity(msg.sender, actualAmountA, actualAmountB, liquidity);
 
-        return (amountAIn, amountBIn, liquidity);
+        return (actualAmountA, actualAmountB, liquidity);
     }
 
     function removeLiquidity(uint256 liquidity) external override returns (uint256 amountA, uint256 amountB) {}
